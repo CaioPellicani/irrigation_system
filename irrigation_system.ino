d8c0d4e370201bdf6fb4cbd33f58df7581e5cc14
Caio Pellicani <caiopellicani@hotmail.com>
2022-02-21 14:48:25 -0300

    My final use

--- irrigation_system.ino
+++ irrigation_system.ino
@@ -1,5 +1,14 @@
 #include "IrSystem.h"
 
+#define EXTERNAL_ARDUINO
+//#define INTERNAL_ARDUINO
+
+#define VALV_INTERNAL 8
+#define VALV_FRONT 6 
+#define VALV_BACK 5
+
+enum groups{ INTERNAL_GARDEN, EXTERNAL_GARDEN };
+
 RTC_DS3231 rtc;
 IrSystem irSys;
 
@@ -7,55 +16,77 @@ void setup(){
     Serial.begin( 9600 );
     while( !rtc.begin() );
     if( rtc.lostPower() ){
-        #ifdef TEST
-        rtc.adjust( DateTime(2022, 2, 11, 9, 0, 0 ) );
-        #else
+#ifdef TEST
+        rtc.adjust( DateTime(2022, 2, 11, 13, 0, 0 ) );
+#else
         rtc.adjust( DateTime( F( __DATE__ ), F( __TIME__ ) ) );
-        #endif
+        Serial.print( "Adjusting RTC" );
+#endif
     }
     Serial.print( "RTC Begin at: " );
     Serial.println( rtc.now().timestamp( DateTime::TIMESTAMP_FULL ) );
-
+    
     irSys.monthlyPercent(  16.69, 30.66, 51.82, 82.12, 91.97, 93.43, 
                           100.00, 99.64, 89.05, 59.49, 40.51, 10.22 );
 
-    #ifdef TEST
+#ifdef TEST
     uint16_t fiveMin = 5;
-    #else
+#else
     uint16_t fiveMin = 5 * 60 * 60;
-    #endif
+#endif
 
-    irSys.addValv( 7, 0 );
-    irSys.addValv( 8, 1 );
-    irSys.addValv( 9, 0 );
-
-    irSys.getValv( 7, 0 )->addConfig( Config( 9, 0, 0, fiveMin, true, 0, ENQUEUED ) );
-    irSys.getValv( 7, 0 )->addConfig( Config( 9, 0, 7, fiveMin, true, 0, ENQUEUED ) );
-    irSys.getValv( 8, 1 )->addConfig( Config( 9, 0, 2, fiveMin, true, 0, ENQUEUED ) ); 
-    irSys.getValv( 9, 0 )->addConfig( Config( 9, 0, 1, fiveMin, true, 0, ENQUEUED ) );
-    irSys.deactivateGroup( 1 );
+    irSys.addValv( VALV_INTERNAL, INTERNAL_GARDEN )->addConfig( 
+        Config( 9, 0, 0, 
+                fiveMin, 
+                false, 
+                10, 
+                ENQUEUED 
+        ) 
+    );
+    irSys.addValv( VALV_FRONT, EXTERNAL_GARDEN )->addConfig( 
+        Config( 9, 0, 1, 
+                fiveMin, 
+                true, 
+                10, 
+                ENQUEUED 
+        ) 
+    );
+    irSys.addValv( VALV_BACK, EXTERNAL_GARDEN )->addConfig( 
+        Config( 9, 0, 2, 
+                fiveMin, 
+                true, 
+                10, 
+                ENQUEUED 
+        ) 
+    );
+#ifdef EXTERNAL_ARDUINO
+    irSys.deactivateGroup( INTERNAL_GARDEN );
+#endif
+#ifdef INTERNAL_ARDUINO
+    irSys.deactivateGroup( EXTERNAL_GARDEN );
+#endif
 }
 
 void loop(){
     irSys.run( rtc.now() );
+    
     static uint32_t lastMillis = millis();
     if( millis() - lastMillis >= 1000 ){
         digitalWrite( LED_BUILTIN, !digitalRead( LED_BUILTIN ) );
+        lastMillis = millis();
     }
 
-    static int map[3] = { LOW, LOW, LOW };
-    for( int i = 0; i < 3; i++ ){
-        if( digitalRead( i + 7 ) != map[i] ){
-            map[i] = digitalRead( i + 7 );
+#ifdef TEST
+    static int map[14] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    for( int i = 0; i < 14; i++ ){
+        if( digitalRead( i ) != map[i] ){
+            map[i] = digitalRead( i );
             Serial.println( rtc.now().timestamp( DateTime::TIMESTAMP_FULL ) );
-            Serial.print( i + 7 );
+            Serial.print( i );
             Serial.print( " -> " );
             Serial.println( map[i] );
-            #ifdef TEST
-            static int interations = 8; if( --interations <= 0 ) exit(0); 
-            #endif
+            static int interations = 100; if( --interations <= 0 ) exit(0); 
         }
     }
-
-
+#endif
 }   
